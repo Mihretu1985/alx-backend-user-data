c51bb567fa12492c61d33a6fbcb518ed8a887ad6
Mihretu1985 <mthaile1985@gmail.com>
2024-05-31 07:10:59 -0700

    updated

--- encrypt_password.py
+++ encrypt_password.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 """
-Password Encryption and Validation Module
+Encrypting passwords
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """
-        Generates a salted and hashed password.
-
-        Args:
-                password (str): A string containing the plain text
-                password to be hashed.
-
-        Returns:
-                bytes: A byte string representing the salted, hashed password.
-        """
+    """ Returns a salted, hashed password, which is a byte string """
     encoded = password.encode()
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
@@ -23,19 +14,7 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """
-        Validates whether the provided password matches the hashed password.
-
-        Args:
-                hashed_password (bytes): A byte string representing
-                the salted, hashed password.
-                password (str): A string containing the plain text
-                password to be validated.
-
-        Returns:
-                bool: True if the provided password matches the hashed
-                password, False otherwise.
-        """
+    """ Validates the provided password matches the hashed password """
     valid = False
     encoded = password.encode()
     if bcrypt.checkpw(encoded, hashed_password):
